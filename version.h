8655983fefc1203b9beb07902e28c4cea862a184
Hassan Elsayed <hassan.elsaied@mint-ops.net>
2026-01-15 13:28:01 +0200

    Auto-update: Version update on branch development

--- version.h
+++ version.h
@@ -1,9 +1,9 @@
 #ifndef VERSION_H
 #define VERSION_H
 #define CURRENT_BRANCH "newDesignSystem"
-const char *HEAD -> main_version[] = {"version:1.2", "Date:15-01-2026", "Time:13:24:31"};
-const char *development_version[] = {"version:1.2", "Date:15-01-2026", "Time:13:24:31"};
-const char *main_version[] = {"version:1.2", "Date:15-01-2026", "Time:13:24:31"};
-const char *newDesignSystem_version[] = {"version:1.1", "Date:15-01-2026", "Time:13:24:31"};
-#define PROJECT_VERSION "1.1"
+const char *HEAD -> main_version[] = {"version:1.3", "Date:15-01-2026", "Time:13:27:46"};
+const char *development_version[] = {"version:1.3", "Date:15-01-2026", "Time:13:27:46"};
+const char *main_version[] = {"version:1.3", "Date:15-01-2026", "Time:13:27:46"};
+const char *newDesignSystem_version[] = {"version:1.2", "Date:15-01-2026", "Time:13:27:46"};
+#define PROJECT_VERSION "1.2"
 #endif
